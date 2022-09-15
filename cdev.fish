89b2ff683af26420195b89d1a7d0808efa96af7d
creator54 <hi.creator54@gmail.com>
2022-09-16 02:22:45 +0530

    set WORKDIR -> /tmp/CP

--- cdev.fish
+++ cdev.fish
@@ -2,10 +2,12 @@
 
 function cdev
   if not set -q argv[1]
-    touch code.cpp inputs output
-    printf '#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n  cout<<"Hello World";\n}' > code.cpp
-    kitty --session cp_session
-    rm -rf code.cpp inputs output
+    set WORKDIR /tmp/CP
+    mkdir -p $WORKDIR
+    touch $WORKDIR/code.cpp $WORKDIR/inputs $WORKDIR/output
+    printf '#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n  cout<<"Hello World";\n}' > $WORKDIR/code.cpp
+    kitty --session (cd (dirname (status -f)); and pwd)/cp_session
+    rm -rf $WORKDIR
   else
     set -x src (echo (pwd)/$argv) #without -x, i.e export entr won't be able to read the shell vaiable
     if echo $src | grep ".cpp\|.cxx" &>/dev/null
