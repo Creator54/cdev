d2fd7580f2dc14098006398d91aacbc6ff699a7e
creator54 <hi.creator54@gmail.com>
2022-09-15 14:10:51 +0530

    fix: input bugs

--- cdev.fish
+++ cdev.fish
@@ -2,10 +2,10 @@
 
 function cdev
   if not set -q argv[1]
-    touch code.cpp inputs
-    printf '#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n\tcout<<"Hello World";\n}' > code.cpp
+    touch code.cpp input output
+    printf '#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n  cout<<"Hello World";\n}' > code.cpp
     kitty --session cp_session
-    rm -rf code.cpp inputs
+    rm -rf code.cpp input output
   else
     set -x src (echo (pwd)/$argv) #without -x, i.e export entr won't be able to read the shell vaiable
     if echo $src | grep ".cpp\|.cxx" &>/dev/null
