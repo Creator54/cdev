6cc25b6370f9a244af2a5df9c2219243aebb5898
creator54 <hi.creator54@gmail.com>
2022-09-18 15:50:23 +0530

    reformatting + cd to initial dir

--- cdev.fish
+++ cdev.fish
@@ -1,17 +1,20 @@
 function cdev
+  set -gx CURRENTDIR (pwd)
+  set -gx WORKDIR $CURRENTDIR
   if not set -q argv[1]
-    set WORKDIR /tmp/CP
-    mkdir -p $WORKDIR
-    touch $WORKDIR/code.cpp $WORKDIR/inputs $WORKDIR/output
-    printf '#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n  ios_base::sync_with_stdio(0);\n  cin.tie(0);\n  cout.tie(0);\n  cout<<"Hello World";\n}' > $WORKDIR/code.cpp
-    kitty --session (cd (dirname (status -f)); and pwd)/cp_session
-    rm -rf $WORKDIR
+    set -gx WORKDIR /tmp/CP
+    set -gx code $WORKDIR/code.cpp
   else
-    set -x src (echo (pwd)/$argv) #without -x, i.e export entr won't be able to read the shell vaiable
-    if echo $src | grep ".cpp\|.cxx" &>/dev/null
-      ls $src | entr -c sh -c 'printf "Output :\n\n" && g++ $src -o temp && ./temp && rm -rf temp'
-    else
-      ls $src | entr -c sh -c 'printf "Output :\n\n" && gcc $src -o temp && ./temp && rm -rf temp'
-    end
+    set -gx code $WORKDIR/$argv[1]
   end
+
+  set -gx input $WORKDIR/inputs
+  set -gx output $WORKDIR/output
+
+  mkdir -p $WORKDIR
+  touch $code $input $output
+  printf '#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n  ios_base::sync_with_stdio(0);\n  cin.tie(0);\n  cout.tie(0);\n  cout<<"Hello World";\n}' > $code
+  kitty --session (cd (dirname (status -f)); and pwd)/cp_session
+  cd $CURRENTDIR
+  set -e CURRENTDIR WORKDIR code input output
 end
